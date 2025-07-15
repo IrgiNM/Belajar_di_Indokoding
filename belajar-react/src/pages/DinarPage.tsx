d757fd4fa733c71c25ce91e254b4a7dcee1ebe0f
Irfan <muhammadirfan2823@gmail.com>
2025-07-15 12:36:51 +0700

    set up tailwind and add example

---
+++ belajar-react/src/pages/DinarPage.tsx
@@ -0,0 +1,10 @@
+// install extension ES7 React/Redux/GraphQL/React-Native snippets di VSCode
+// extension ini menyediakan snippet untuk membuat komponen React dengan cepat
+// cara pemakaian: ketika pertama kali buka halaman, bisa langsung ketik rfc atau rfce dan enter
+
+// ini contoh menggunakan rfc
+import React from "react";
+
+export default function DinarPage() {
+  return <div style={{ color: "red" }}>DinarPage</div>;
+}
